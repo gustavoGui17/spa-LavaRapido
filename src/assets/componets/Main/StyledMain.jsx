408bca136c49a24f719f664f23c64632140144ed
Gustavo Guilherme <gustavo.gsilva1723@gmail.com>
2025-08-16 01:09:00 -0300

    Finished Main

---
+++ src/assets/componets/Main/StyledMain.jsx
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import image from "../../img/image.png"
+
+const StyledContainer = styled.section`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 40px;
+  background-color: white;
+  gap: 30px;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+`
+
+const StyledImage = styled.div`
+  flex: 1;
+  img {
+    width: 70%;
+    height: auto;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+`
+
+const StyledContent = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  right: 150px;
+`
+
+const StyledTitle = styled.h1`
+  font-size: 28px;
+  font-weight: bold;
+`
+
+const StyledSubtitle = styled.h3`
+  font-size: 22px;
+  font-weight: bold;
+  margin-bottom: 15px;
+`
+
+const Styledtext = styled.p`
+  line-height: 1.6;
+  margin-bottom: 15px;
+`
+
+const StyledHighlight = styled.p`
+  color: #0056d2;
+  font-weight: bold;
+  margin-bottom: 20px;
+`
+
+const StyledContact = styled.div`
+  font-size: 14px;
+  margin-bottom: 20px;
+`
+
+export default function StyledMain() {
+    return (
+        <StyledContainer>
+            <StyledImage>
+                <img src={image} alt="Enricar" />
+            </StyledImage>
+
+            <StyledContent>
+                <StyledTitle>About EnriCar</StyledTitle>
+                <StyledSubtitle>Lavagem manual e detalhamento de carros</StyledSubtitle>
+
+                <Styledtext>Bem-vindo à EnriCar, onde redefinimos a arte de cuidar de carros <br /> com nossos serviços incomparáveis de lavagem manual. Na EnriCar, <br /> acreditamos em mais do que apenas limpar veículos; acreditamos em <br /> realçar e preservar sua beleza, um detalhe meticuloso de cada vez.
+                </Styledtext>
+
+                <Styledtext>Na EnriCar, nossa missão não é apenas limpar carros; é elevar <br /> toda a experiência de cuidar de um carro. Somos movidos pela paixão, <br /> guiados pela integridade e dedicados a fazer cada veículo brilhar
+                </Styledtext>
+
+                <StyledHighlight>
+                    – porque acreditamos que cada viagem merece ser uma obra-prima.
+                </StyledHighlight>
+
+                <StyledContact>
+                    <p>📍 Elizabeth Shopping Centre, Next To Coles, Adelaide, SA, Australia</p>
+                    <p>📞 +61 433 457 269</p>
+                    <p>📧 washmasterelizabeth@gmail.com</p>
+                </StyledContact>
+
+            </StyledContent>
+        </StyledContainer>
+    )
+}
--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import StyledNavbar from "../../componets/Navbar/StyledNavbar";
 import StyledBanner from "../../componets/Navbar/StyledBanner";
 import StyledPricing from "../../componets/Cards/StyledPricing";
+import StyledMain from "../../componets/Main/StyledMain";
 
 export default function Home(){
     return (
@@ -8,6 +9,7 @@ export default function Home(){
             <StyledNavbar/>
             <StyledBanner/>
             <StyledPricing/>
+            <StyledMain/>
         </div>
     )
 }
